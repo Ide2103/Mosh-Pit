dbb9bc4633863c81f35dd1c05cd3fc408406d9b0
Ide2103 <107347502+Ide2103@users.noreply.github.com>
2022-09-08 01:23:30 +0000

    Initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "a38afe4faa91484cbc41372548f7e76e.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
