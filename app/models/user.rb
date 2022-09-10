97f832496f9b982378b7fd0923da853047bf861b
Ide2103 <107347502+Ide2103@users.noreply.github.com>
2022-09-10 12:24:55 +0000

    header

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :posts, dependent: :destroy
-  
-  
-  
-  
+  has_one_attached :iconimage
+
+
+
+
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -16,5 +17,5 @@ class User < ApplicationRecord
       user.name = "ゲスト"
     end
   end
-  
+
 end
