47f6fa0468c93405bac24c5156b5d20d1ea70627
Ide2103 <107347502+Ide2103@users.noreply.github.com>
2022-09-22 15:48:40 +0000

    chat

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,12 +3,13 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :comment, presence: true, length: {maximum: 150}
-  
+
   def self.search_for(content, method)
+    print(method)
     if method == 'perfect'
-      PostComment.where(comment: content)
+      PostComment.where(comment: content.to_s)
     else
-      PostComment.where('comment LIKE ?', '%'+content+'%')
+      PostComment.where('comment LIKE ?', "%" + content.to_s + "%")
     end
   end
 end
